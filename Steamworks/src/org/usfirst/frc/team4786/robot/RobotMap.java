3a69c4b6dad546e02c484a031333fe6c84b8126a
Fear4 <Fear4@172.18.7.236>
2017-01-10 18:50:08 -0600

    Added Initial PID DriveCode, assumes 4 motors (2 per side)

--- Steamworks/src/org/usfirst/frc/team4786/robot/RobotMap.java
+++ Steamworks/src/org/usfirst/frc/team4786/robot/RobotMap.java
@@ -7,13 +7,57 @@ package org.usfirst.frc.team4786.robot;
  * floating around.
  */
 public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
+	
+	//CANTalon ports
+	// use instead of magic numbers
+	//are final ports
+	
+	public static final int backLeftPort = 15;
+	public static final int backRightPort = 12;
+	public static final int frontLeftPort = 14;
+	public static final int frontRightPort = 13;
+	public static final int intakeMotorPort = 2;
+	public static final int positionMotorPort = 3;
+	public static final int limitSwitchTopPort = 8;
+	public static final int limitSwitchBottomPort = 7;
+	public static final int limitSwitchIntakePort = 6;
+	
+	//game mech constants
+	//use instead of hard-coded values
+	public static final double armSpeed = .5;
+	public static final double intakeSpeed = -1;
+	public static final double passSpeed = .75;
+	public static final double shootSpeed = 1;
+	public static final double raisePortcullisSpeed = 1;
 
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
+	//autonomous constants
+	//use instead of hard-coded numbers
+	public static final double lowBarSpeed = 0.6;
+	public static final double moatSpeed = 0.6;
+	public static final double rampartsSpeed = 0.5;
+	public static final double rockyTerrainSpeed = 0.7;
+	public static final double rockWallSpeed = 0.6;
+	public static final double autoSpeed = 0.5;
+	
+	public static final double autoTimer = 5;
+	public static final double moatTimer = 4;
+	public static final double rockWallTimer = 3;
+	public static final double rockyTerrainTimer = 4;
+	public static final double rampartsTimer = 3;
+	public static final double lowBarTimer = 4;
+	
+	
+	public static final double scaling = .6;
+
+	
+	public static final double defaultArmPosition = 0;
+
+	
+	
+    /* For example to map the left and right motors, you could define the
+     following variables to use with your drivetrain subsystem.
+     If you are using multiple modules, make sure to define both the port
+     number and the module. 
+     
+     */
 }
