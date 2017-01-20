0d13b00fdb6a3149cd8e7126a145d47380f638d6
Noah-Kennedy <Nomaxx117@gmail.com>
2017-01-19 19:37:13 -0600

    Arduino working code for send string, test code for recieve string.

--- Steamworks/src/org/usfirst/frc/team4786/robot/subsystems/Arduino.java
+++ Steamworks/src/org/usfirst/frc/team4786/robot/subsystems/Arduino.java
@@ -8,7 +8,10 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class Arduino extends Subsystem {
-	private static I2C Wire = new I2C(Port.kOnboard, 4);
+	private static I2C wire;
+	public Arduino(){
+		wire = new I2C(Port.kOnboard, 4);
+	}
 
 
 
@@ -28,7 +31,26 @@ public class Arduino extends Subsystem {
     	for (int i = 0; i < CharArray.length; i++) {
     		WriteData[i] = (byte) CharArray[i];
     	}
-    	Wire.transaction(WriteData, WriteData.length, null, 0);
+    	wire.transaction(WriteData, WriteData.length, null, 0);
+    }
+    
+    public String readStringData(){
+    	byte[] byteLength = new byte[2];
+    	wire.transaction(null, 0, byteLength, 2);
+    	char[] lengthCharArray = new char[2];
+    	for(int i = 0; i < lengthCharArray.length; i++){
+    		lengthCharArray[i] = (char) byteLength[i];
+    	}
+    	String lenString = String.valueOf(lengthCharArray);
+    	int length = Integer.parseInt(lenString);
+    	byte[] data = new byte[length];
+    	char[] charArray = new char[length];
+    	wire.transaction(null, 0, data, length);
+    	for(int i = 0; i < length; i++){
+    		charArray[i] = (char) data[i];
+    	}
+    	String dataString = String.valueOf(charArray);
+    	return dataString;
     }
 }
 
