5515faf9bf7369b01fd99e24e489046df0a1b5b9
Jake Secunda <secundaj@nicoletfear.com>
2017-02-04 13:30:25 -0600

    Initial OpenClimb system, untested; Test DriveBase PIDCode, untested;Fix

--- Steamworks/src/org/usfirst/frc/team4786/robot/Robot.java
+++ Steamworks/src/org/usfirst/frc/team4786/robot/Robot.java
@@ -79,6 +79,8 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void autonomousInit() {
 
+		autonomousCommand = new DriveToPosition(10);
+		
 		/*
 		 * String autoSelected = SmartDashboard.getString("Auto Selector",
 		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
@@ -123,10 +125,6 @@ public class Robot extends IterativeRobot {
 		Scheduler.getInstance().run();
 		SmartDashboard.putNumber("Left Motor Output", driveTrain.motorOutputLeft);
 		SmartDashboard.putNumber("Right Motor Output", driveTrain.motorOutputRight);
-		SmartDashboard.putNumber("Left Encoder Position", driveTrain.frontLeft.getEncPosition());
-		SmartDashboard.putNumber("Right Encoder Position", driveTrain.frontRight.getEncPosition());
-		SmartDashboard.putNumber("Left Encoder Velocity", driveTrain.frontLeft.getEncVelocity());
-		SmartDashboard.putNumber("Right Encoder Velocity", driveTrain.frontLeft.getEncVelocity());
 		SmartDashboard.putBoolean("Gear Present", Gear.gearLimitSwitchPressed());
 		SmartDashboard.putBoolean("Peg Present", Gear.pegLimitSwitchPressed());
 	}
