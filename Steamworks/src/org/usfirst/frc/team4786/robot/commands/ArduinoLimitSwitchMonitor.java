ca9c6ef886fba795b69b291a5e5bbe6c36bab3ab
gnguralnick <gnguralnick@gmail.com>
2017-02-04 18:34:52 -0600

    Made RoboRio side work. No longer calling command, calling method. Arduino side still not functional.

--- Steamworks/src/org/usfirst/frc/team4786/robot/commands/ArduinoLimitSwitchMonitor.java
+++ Steamworks/src/org/usfirst/frc/team4786/robot/commands/ArduinoLimitSwitchMonitor.java
@@ -10,8 +10,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class ArduinoLimitSwitchMonitor extends Command {
-	
-    public ArduinoLimitSwitchMonitor() {
+	String last = "red";
+ 
+	public ArduinoLimitSwitchMonitor() {
         requires(Robot.gear);
     }
 
@@ -22,21 +23,29 @@ public class ArduinoLimitSwitchMonitor extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	RedLight red = new RedLight();
-    	BlueLight blue = new BlueLight();
+    	//RedLight red = new RedLight();
+    	//BlueLight blue = new BlueLight(); 
 		
-    	if(Gear.pegLimitSwitchPressed() == false){
+    	if(Gear.pegLimitSwitchPressed() == false && !last.equals("blue")){
     		Robot.oi.getXbox().setRumble(RumbleType.kLeftRumble, 0);
     		Robot.oi.getXbox().setRumble(RumbleType.kRightRumble, 0);
-    		blue.start();
+    		//blue.start();
+    		last = "blue";
+        	Robot.arduino.writeStringData("bluelight");
+
       		}	
 	
 
-    	if(Gear.pegLimitSwitchPressed() == true){
-    		SmartDashboard.putString("Working", "j");
+    	if(Gear.pegLimitSwitchPressed() == true && !last.equals("red")){
+    		SmartDashboard.putString("Working", "test");
     		Robot.oi.getXbox().setRumble(RumbleType.kLeftRumble, 1);
     		Robot.oi.getXbox().setRumble(RumbleType.kRightRumble, 1);
-    		red.start();
+    		last = "red";
+    		//red.start();
+        	Robot.arduino.writeStringData("redlight");
+    		
+    		
+    		
         	
     	}
 
