9b01c4fdef6144cc3bc32ef7ca96584b053c8845
Noah-Kennedy <Nomaxx117@gmail.com>
2017-02-11 22:45:07 -0600

    starting to put vision processing back into its own class

--- Steamworks/src/org/usfirst/frc/team4786/robot/Robot.java
+++ Steamworks/src/org/usfirst/frc/team4786/robot/Robot.java
@@ -125,93 +125,9 @@ public class Robot extends IterativeRobot {
 				regStream.putFrame(mat.getMat());
 
 				//Core.inRange(mat.getMat(), new Scalar(250,250,250), new Scalar(255,255,255), mat.getMat());
-
-				//BGR
-				Core.inRange(mat.getMat(), new Scalar(RobotMap.lowBlueValue,RobotMap.lowGreenValue,RobotMap.lowRedValue), new Scalar(RobotMap.highBlueValue,RobotMap.highGreenValue,RobotMap.highRedValue), mat.getMat());
 				
-				Mat hierarchy = new Mat();
-        		List<MatOfPoint> contours = new ArrayList<>();
-        		
-        		//filtered camera feed won't show filtered objects after this - until draw contours
-        		findContours(mat.getMat(), contours, hierarchy, RETR_LIST, CHAIN_APPROX_NONE);
-        		
-        		List<MatOfPoint> filteredContours = new ArrayList<>();	//List of filtered contours
-        		List<Rect> filteredContoursRect = new ArrayList<>();
-        		double largestRectArea = 0;
-        		double smallestRectArea = 100;
-        		
-        		for (MatOfPoint contour : contours)	{ //for every contour(Matrix of points) in contours
-        			Rect boundingRect = boundingRect(contour);		//creates a rectangle around the contour
-        			//double rectRatio = (boundingRect.height*1.0)/boundingRect.width;
-        			//double ratioOfTape = 5/2;	//it will be 5/2 in competition
-        			//double errorMargin = .25;
-        			//filtering out false positives - contour must be taller than it is wide 
-        			// & the ratio of the target's height over width must be = to the tape's height over width with a margin of error
-        			if(boundingRect.width < boundingRect.height && boundingRect.area() >= RobotMap.minPixelCount) {//&& (((1 - errorMargin)*(ratioOfTape)) < rectRatio && rectRatio < (1 + errorMargin)*(ratioOfTape))) 
-        				filteredContours.add(contour); 	//adds contours that fit requirements to list of contours
-        				filteredContoursRect.add(boundingRect);
-        				//contoursFound = true;
-        				//draws filtered contours on video display
-        				drawContours(mat.getMat(), filteredContours, -1, new Scalar(0xFF, 0xFF, 0xFF), FILLED);
-        				if(boundingRect.area() > largestRectArea)
-        					largestRectArea = boundingRect.area();
-        				if(boundingRect.area() < smallestRectArea)
-        					smallestRectArea = boundingRect.area();
-        			}
-        		}
-        		//double centerY = (leftRect.y + leftRect.height * .5);
-        		//Imgproc.circle(mat.getMat(), new Point(centerX,centerY), 20, new Scalar(255,255,255), 2);
-        		//Imgproc.rectangle(mat.getMat(), new Point(leftRect.x,leftRect.y), new Point(rightRect.x,rightRect.y), new Scalar(255,255,255),2);
-        		
-				// Give the output stream a new image to display
-				outputStream.putFrame(mat.getMat());
-				boolean twoTargets = false;
-				int numOfTargets = 0;
-				Rect leftRect;
-				Rect rightRect;
-				double distanceToLeft;
-				double distanceToRight;
-				double matHeight = mat.getHeight();
-				//(widthOfTarget*pixelFieldOfViewWidth)/(pixelWidthOfTarget*(.5*fieldOfViewWidth)/distanceAtCalibration)-distanceOfCamFromFrontOfBot;
-				if(filteredContours.size() >= 2){
-					twoTargets = true;
-				}
-				numOfTargets = filteredContours.size();
-				if(twoTargets){
-					
-					if(filteredContoursRect.get(0).x < filteredContoursRect.get(1).x){
-						leftRect = filteredContoursRect.get(0);
-						rightRect = filteredContoursRect.get(1);
-					}else{
-						leftRect = filteredContoursRect.get(1);
-						rightRect = filteredContoursRect.get(0);
-					}
-					distanceToLeft = RobotMap.heightOfTargetInFeet * matHeight;
-					distanceToLeft /= (leftRect.height*(.5*RobotMap.cameraFOVHeightInFeet)/RobotMap.distanceAtCalibration);
-					distanceToLeft -= RobotMap.distanceOfCamFromFrontOfBot;
-					/*distanceToLeft = ((RobotMap.heightOfTargetInFeet*matHeight)/
-							(leftRect.height*(.5*RobotMap.cameraFOVHeightInFeet)/RobotMap.distanceAtCalibration))-RobotMap.distanceOfCamFromFrontOfBot;*/
-					distanceToRight = ((RobotMap.heightOfTargetInFeet*matHeight)/
-							(rightRect.height*(.5*RobotMap.cameraFOVHeightInFeet)/RobotMap.distanceAtCalibration))-RobotMap.distanceOfCamFromFrontOfBot;
-					distanceToLeft += .088;
-					distanceToRight += .088;
-					distanceToLeft /= 1.886;
-					distanceToRight /= 1.886;
-	        		double centerX = .5 * ((leftRect.x + leftRect.width) + rightRect.x);
-					SmartDashboard.putNumber("Left Height", leftRect.height);
-					SmartDashboard.putNumber("Right Height", rightRect.height);
-					SmartDashboard.putNumber("Left Width", leftRect.width);
-					SmartDashboard.putNumber("Right Width", rightRect.width);
-					SmartDashboard.putNumber("Distance to left Rect", distanceToLeft);
-					SmartDashboard.putNumber("Distance to right Rect", distanceToRight);
-				}
-
-				SmartDashboard.putBoolean("Rectangles detected?", twoTargets);
-				SmartDashboard.putNumber("Number of targets", numOfTargets);
-				SmartDashboard.putNumber("Area of largest Rect", largestRectArea);
-				SmartDashboard.putNumber("Area of smallest Rect", smallestRectArea);
-				//SmartDashboard.putNumber("X", filteredContoursRect.get(0).x);	un-commenting this results in too many simultaneous client streams
-				//SmartDashboard.putNumber("Y", filteredContoursRect.get(0).y);	un-commenting this results in too many simultaneous client streams
+				//BGR
+				
 				
 			}
 			} catch (Exception e) {
--- Steamworks/src/org/usfirst/frc/team4786/robot/subsystems/VisionImage.java
+++ Steamworks/src/org/usfirst/frc/team4786/robot/subsystems/VisionImage.java
@@ -12,129 +12,106 @@ import static org.opencv.core.Core.*;
 import org.opencv.videoio.VideoCapture;
 import org.usfirst.frc.team4786.robot.Robot;
 import org.usfirst.frc.team4786.robot.RobotMap;
+import org.usfirst.frc.team4786.robot.subsystems.MatRapper;
 
-public class VisionImage implements VisionPipeline {	
+public class VisionImage {	
+	//everything is static
+	static int numOfTargets = 0;
+	static double largestRectArea = 0;
+	static double smallestRectArea = 100;
+	static Rect leftRect;
+	static Rect rightRect;
+	static double distanceToLeft;
+	static double distanceToRight;
 	
-	//these fields are for methods in FrameData
-	private boolean contoursFound = false;	//have contours been found
-	private int numOfTargets;
-	private double widthOfTarget;	//in pixels
-	private int fieldOfViewWidth;	//in pixels
-	private double rightContourArea;
-	private double leftContourArea;
-	
-	public Mat filtered = new Mat();
-	
-	/*	put in robot class - robotInnit()
-	Mat image = new Mat();
-	VideoCapture camera = new VideoCapture(0);
-	camera.read(image);
-	*/
-	
-	public boolean getContoursFound(){
-		boolean f = contoursFound;
-		return f;
-	}
-	
-	public int getNumberOfTargets(){
-		int n = numOfTargets;
-		return n;
-	}
-	
-	public double getWidthOfTarget(){
-		double w = widthOfTarget;
-		return w;
-	}
-	
-	public int getFieldOfViewWidth(){
-		int w = fieldOfViewWidth;
-		return w;
-	}
-	
-	public double getRightContourArea(){
-		double a = rightContourArea;
-		return a;
-	}
-	
-	public double getLeftContourArea(){
-		double a = leftContourArea;
-		return a;
-	}
-	
-	public void process(Mat image) {
+	public static void processImage(MatRapper mat){
+		Core.inRange(mat.getMat(), new Scalar(RobotMap.lowBlueValue,RobotMap.lowGreenValue,RobotMap.lowRedValue), new Scalar(RobotMap.highBlueValue,RobotMap.highGreenValue,RobotMap.highRedValue), mat.getMat());
 		
+		Mat hierarchy = new Mat();
+		List<MatOfPoint> contours = new ArrayList<>();
 		
-		//try statements ensure the MatRapper class releases the memory upon each iteration
-        try(MatRapper blurred = new MatRapper(new Mat())) {
-        	//blurs image to smooth out false positives
-        	GaussianBlur(image, blurred.getMat(), new Size(320, 240), 0);
-        	try(MatRapper processLocalFiltered = new MatRapper(image)) {
-        		//for getDistance() in FrameData
-        		fieldOfViewWidth = processLocalFiltered.getWidth();
-        		
-        		double lowB = RobotMap.lowBlueValue;
-        		double lowR = RobotMap.lowRedValue;
-        		double lowG = RobotMap.lowGreenValue;
-        		double highB = RobotMap.highBlueValue;
-        		double highR = RobotMap.highRedValue;
-        		double highG = RobotMap.highGreenValue;
-        		//values from image, R:251 G:252 B:250
-        		
-        		//filters out contours not within color range
-        		inRange(blurred.getMat(), new Scalar(lowR, lowG, lowB), new Scalar(highR, highG, highB), processLocalFiltered.getMat());	
-        		
-        		Mat hierarchy = new Mat();
-        		List<MatOfPoint> contours = new ArrayList<>();
-        		
-        		findContours(processLocalFiltered.getMat(), contours, hierarchy, RETR_LIST, CHAIN_APPROX_NONE);
-        		
-        		List<MatOfPoint> filteredContours = new ArrayList<>();	//List of filtered contours
-        		List<Rect> filteredContoursRect = new ArrayList<>();	//List of filtered contours as Rect objects
-        		
-        		for (MatOfPoint contour : contours)	{ //for every contour(Matrix of points) in contours
-        			Rect boundingRect = boundingRect(contour);		//creates a rectangle around the contour
-        			//double rectRatio = (boundingRect.height*1.0)/boundingRect.width;
-        			//double ratioOfTape = 5/2;	//it will be 5/2 in competition
-        			//double errorMargin = .25;
-        			//filtering out false positives - contour must be taller than it is wide 
-        			// & the ratio of the target's height over width must be = to the tape's height over width with a margin of error
-        			if(boundingRect.width < boundingRect.height ) {//&& (((1 - errorMargin)*(ratioOfTape)) < rectRatio && rectRatio < (1 + errorMargin)*(ratioOfTape))) 
-        				filteredContours.add(contour); 	//adds contours that fit requirements to list of contours
-        				filteredContoursRect.add(boundingRect);
-        				contoursFound = true;
-        				//draws filtered contours on video display
-        				drawContours(processLocalFiltered.getMat(), filteredContours, -1, new Scalar(0xFF, 0xFF, 0xFF), FILLED);
-        			}
-        		}
-        		if(filteredContoursRect.get(0).br().x < filteredContoursRect.get(1).br().x) {
-        			//if the x value of first filtered contour < second filtered contour
-        			leftContourArea = filteredContoursRect.get(0).area();
-        			rightContourArea = filteredContoursRect.get(1).area();
-        			widthOfTarget = filteredContoursRect.get(0).width;
-        		}else{
-        			leftContourArea = filteredContoursRect.get(1).area();
-        			rightContourArea = filteredContoursRect.get(0).area();
-        			widthOfTarget = filteredContoursRect.get(1).width;
-        		}
-        		
-        		//For Testing
-        		//SmartDashboard.putNumber("Distance", Robot.frameData.getDistance());
-        		//SmartDashboard.putString("Location of Target", Robot.frameData.getLocationOfTarget().name());
-        		filtered = blurred.getMat();
-        }catch (Exception e) {
-        	e.printStackTrace();
-        }
-        }catch (Exception e1) {
-        	e1.printStackTrace();
-        }
+		//filtered camera feed won't show filtered objects after this - until draw contours
+		findContours(mat.getMat(), contours, hierarchy, RETR_LIST, CHAIN_APPROX_NONE);
+		
+		List<MatOfPoint> filteredContours = new ArrayList<>();	//List of filtered contours
+		List<Rect> filteredContoursRect = new ArrayList<>();
+		
+		for (MatOfPoint contour : contours)	{ //for every contour(Matrix of points) in contours
+			Rect boundingRect = boundingRect(contour);		//creates a rectangle around the contour
+			//double rectRatio = (boundingRect.height*1.0)/boundingRect.width;
+			//double ratioOfTape = 5/2;	//it will be 5/2 in competition
+			//double errorMargin = .25;
+			//filtering out false positives - contour must be taller than it is wide 
+			// & the ratio of the target's height over width must be = to the tape's height over width with a margin of error
+			if(boundingRect.width < boundingRect.height && boundingRect.area() >= RobotMap.minPixelCount) {//&& (((1 - errorMargin)*(ratioOfTape)) < rectRatio && rectRatio < (1 + errorMargin)*(ratioOfTape))) 
+				filteredContours.add(contour); 	//adds contours that fit requirements to list of contours
+				filteredContoursRect.add(boundingRect);
+				//contoursFound = true;
+				//draws filtered contours on video display
+				drawContours(mat.getMat(), filteredContours, -1, new Scalar(0xFF, 0xFF, 0xFF), FILLED);
+				if(boundingRect.area() > largestRectArea)
+					largestRectArea = boundingRect.area();
+				if(boundingRect.area() < smallestRectArea)
+					smallestRectArea = boundingRect.area();
+			}
+		}
+		//double centerY = (leftRect.y + leftRect.height * .5);
+		//Imgproc.circle(mat.getMat(), new Point(centerX,centerY), 20, new Scalar(255,255,255), 2);
+		//Imgproc.rectangle(mat.getMat(), new Point(leftRect.x,leftRect.y), new Point(rightRect.x,rightRect.y), new Scalar(255,255,255),2);
+		
+		// Give the output stream a new image to display
+		//outputStream.putFrame(mat.getMat());
+		boolean twoTargets = false;
+		
+		double matHeight = mat.getHeight();
+		//(widthOfTarget*pixelFieldOfViewWidth)/(pixelWidthOfTarget*(.5*fieldOfViewWidth)/distanceAtCalibration)-distanceOfCamFromFrontOfBot;
+		if(filteredContours.size() >= 2){
+			twoTargets = true;
+		}
+		numOfTargets = filteredContours.size();
+		if(twoTargets){
+			
+			if(filteredContoursRect.get(0).x < filteredContoursRect.get(1).x){
+				leftRect = filteredContoursRect.get(0);
+				rightRect = filteredContoursRect.get(1);
+			}else{
+				leftRect = filteredContoursRect.get(1);
+				rightRect = filteredContoursRect.get(0);
+			}
+			distanceToLeft = RobotMap.heightOfTargetInFeet * matHeight;
+			distanceToLeft /= (leftRect.height*(.5*RobotMap.cameraFOVHeightInFeet)/RobotMap.distanceAtCalibration);
+			distanceToLeft -= RobotMap.distanceOfCamFromFrontOfBot;
+			/*distanceToLeft = ((RobotMap.heightOfTargetInFeet*matHeight)/
+					(leftRect.height*(.5*RobotMap.cameraFOVHeightInFeet)/RobotMap.distanceAtCalibration))-RobotMap.distanceOfCamFromFrontOfBot;*/
+			distanceToRight = ((RobotMap.heightOfTargetInFeet*matHeight)/
+					(rightRect.height*(.5*RobotMap.cameraFOVHeightInFeet)/RobotMap.distanceAtCalibration))-RobotMap.distanceOfCamFromFrontOfBot;
+			distanceToLeft += .088;
+			distanceToRight += .088;
+			distanceToLeft /= 1.886;
+			distanceToRight /= 1.886;
+    		double centerX = .5 * ((leftRect.x + leftRect.width) + rightRect.x);
+			SmartDashboard.putNumber("Left Height", leftRect.height);
+			SmartDashboard.putNumber("Right Height", rightRect.height);
+			SmartDashboard.putNumber("Left Width", leftRect.width);
+			SmartDashboard.putNumber("Right Width", rightRect.width);
+			SmartDashboard.putNumber("Distance to left Rect", distanceToLeft);
+			SmartDashboard.putNumber("Distance to right Rect", distanceToRight);
+		}
+
+		SmartDashboard.putBoolean("Rectangles detected?", twoTargets);
+		SmartDashboard.putNumber("Number of targets", numOfTargets);
+		SmartDashboard.putNumber("Area of largest Rect", largestRectArea);
+		SmartDashboard.putNumber("Area of smallest Rect", smallestRectArea);
+		//SmartDashboard.putNumber("X", filteredContoursRect.get(0).x);	un-commenting this results in too many simultaneous client streams
+		//SmartDashboard.putNumber("Y", filteredContoursRect.get(0).y);	un-commenting this results in too many simultaneous client streams
 	}
-	public Mat returnFilteredImage() {	//returns filtered image
-		return filtered;
+	public static double getDistanceLeft(){
+		return distanceToLeft;
 	}
-	/*public int getWidth() {
-		return processLocalFiltered.getWidth();
+	public static double getDistanceRight(){
+		return distanceToRight;
 	}
-	public int getHeight() {
-		//return filtered.getHeight();
-	}*/
+	public static int getNumOfTargets(){
+		return numOfTargets;
+	}
 }
