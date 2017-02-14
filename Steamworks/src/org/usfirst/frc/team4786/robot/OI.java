108fc5ed62d212816a459266dbc894d054626770
Wallace Watler <watlerw@nicoletfear.com>
2017-02-13 20:39:35 -0600

    Added joystick buttons for controlling the side switcher.

--- Steamworks/src/org/usfirst/frc/team4786/robot/OI.java
+++ Steamworks/src/org/usfirst/frc/team4786/robot/OI.java
@@ -1,9 +1,13 @@
 package org.usfirst.frc.team4786.robot;
 
+import org.usfirst.frc.team4786.robot.commands.SwitchFrontSide;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 
 
@@ -20,12 +24,20 @@ public class OI {
 	private final XboxController xbox;
 	public static GenericHID.Hand kLeft;
 	public static GenericHID.Hand kRight;
+	public static Button kLeftJoy6Button;
+	public static Button kRightJoy6Button;
+	public static Button kLeftJoy11Button;
+	public static Button kRightJoy11Button;
 	
     public OI(){
     	//Init the objects for all the buttons, sensors, joysticks, and Xbox controllers
     	leftDriveJoy = new Joystick(0);
     	rightDriveJoy = new Joystick(1);
     	xbox = new XboxController(2);
+    	kLeftJoy6Button = new JoystickButton(leftDriveJoy, 6);
+    	kRightJoy6Button = new JoystickButton(rightDriveJoy, 6);
+    	kLeftJoy11Button = new JoystickButton(leftDriveJoy, 11);
+    	kRightJoy11Button = new JoystickButton(rightDriveJoy, 11);
     	
     	//Tie our many buttons, sensors, joysticks, and Xbox controllers to robot commands
     	    	
@@ -47,14 +59,22 @@ public class OI {
     	} else if (xbox.getBumper(kLeft) == false) {
     		Robot.drawBridge.closeThyBridge();
     	}
-    	//need a button to switch cameras
-    	
-    	//drawbridge closes when false
     	
     	if (xbox.getBumper(kRight) == true) {
     		Robot.intake.collectBalls();
-    		//might not be correct, we kinda just put whatever we found
-    	}
+    	} else if (xbox.getBumper(kRight) == false) {
+    		Robot.intake.stopIntaking();
+    	} 
+    	
+    	//We map a bunch of buttons to switch the front side for Andy's convience ;)
+    	kLeftJoy6Button.whileHeld(new SwitchFrontSide());
+    	kRightJoy6Button.whileHeld(new SwitchFrontSide());
+    	kLeftJoy11Button.whileHeld(new SwitchFrontSide());
+    	kRightJoy11Button.whileHeld(new SwitchFrontSide());
+    	
+    	//need a button to switch cameras
+    	
+    	//drawbridge closes when false
     }
 	
 	public Joystick getLeftDriveJoy() {
