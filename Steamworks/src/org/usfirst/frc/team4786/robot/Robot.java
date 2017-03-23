85187bff05d4f6a3a35a9ae51257c6512256fbc1
Noah-Kennedy <Nomaxx117@gmail.com>
2017-03-23 11:35:53 -0500

    Milwaukee Thursday 11:33 -> Swapped Distances
    Swapped baseline distance with center peg distance because I made a
    mistake earlier and changed the wrong one.

--- Steamworks/src/org/usfirst/frc/team4786/robot/Robot.java
+++ Steamworks/src/org/usfirst/frc/team4786/robot/Robot.java
@@ -92,9 +92,9 @@ public class Robot extends IterativeRobot {
 
 
 		sendableChooser = new SendableChooser<Command>();
-		sendableChooser.addDefault("Drive to Center Gear Peg", new DriveToPosition(7));
+		sendableChooser.addDefault("Drive to Center Gear Peg", new DriveToPosition(9.198-RobotMap.professorElementalRobotLength));
 		sendableChooser.addObject("Do Nothing!", new DoNothing());
-		sendableChooser.addObject("Drive to Baseline", new DriveToPosition(9.198-RobotMap.professorElementalRobotLength));
+		sendableChooser.addObject("Drive to Baseline", new DriveToPosition(10));
 		sendableChooser.addObject("Drive to Left Gear Peg", new DriveToLeftGearPeg());
 		sendableChooser.addObject("Drive to Right Gear Peg", new DriveToRightGearPeg());
 		//sendableChooser.addObject("GetToGearTest", new GearFromOffset());
